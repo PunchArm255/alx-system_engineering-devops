fb0d722ba5d1062c501a67548410fc4158d649c9
Your Name <you@example.com>
2024-04-29 19:53:04 -0700

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# Configures a clean install ubuntu pc
+exec {'update_packages':
+command => '/usr/bin/apt-get update -y'
+}
+
+package {'nginx':
+ensure => installed
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+ensure  => present,
+content => "Hello World!\n"
+}
+
+exec {'add_header':
+command => '/bin/sed -i "/^\tlocation \/ {.*/a \\\t \tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default; /usr/sbin/service nginx restart'
+}
+
+exec {'redirect':
+command => '/bin/sed -i "/^\tserver_name _.*/a \\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default; /usr/sbin/service nginx restart'
+}
+
+file {'/var/www/html/error404.html':
+ensure  => present,
+content => "Ceci n'est pas une page\n"
+}
+
+exec {'error404':
+command => '/bin/sed -i "/^\tserver_name _.*/a \\\terror_page 404 /error404.html;" /etc/nginx/sites-available/default; /usr/sbin/service nginx restart'
+}
