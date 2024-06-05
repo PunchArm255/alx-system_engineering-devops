947f5d04d78c4478e68043329ca7c31f37c12cee
PunchArm255 <simohamednassiri@gmail.com>
2024-06-05 14:35:12 -0700

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# Fix wp config file typo lol
+exec {'fix-wordpress':
+command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+notify  => Service['apache2'],
+}
+
+service {'apache2':
+ensure => 'running',
+}
